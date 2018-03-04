361b9d454134106bec333e4d768ca3cc37673943
VasiliyRepin <36683162+VasiliyRepin@users.noreply.github.com>
2018-03-04 11:30:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,78 +1,55 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-    function getPrimitives(base) {
-        var k = base;
-        var obj = {};
-        for (var i = 2; i <= k; i++) {
-            while (k % i == 0) {
-                if (obj[i]) obj[i]++;
-                else obj[i] = 1;
-                k /= i;
-            }
-        }
-        return obj;
-    }
-
-    var primitiv = getPrimitives(base);
-    var result = [];
-
-    for (var key in primitiv) {
-        var count = 0;
-        var n = number;
-        while (n >= 1) {
-            var temp = Math.floor(n / key);
-            n = temp;
-            count += temp;
-        }
-        result.push(Math.floor(count / primitiv[key]));
-    }
-
-    return Math.min.apply(null, result);
-
-
-}
-/*
+  
 
 
 
-    var primitives=[];
+    var primitives = [];
     var k = 2;
-    function getPrimitives(base)
-    {
-        while(k<=base)
-        {
-            if (base % k==0)
-            {
+
+    function getPrimitives(base) {
+        while (k <= base) {
+            if (base % k == 0) {
                 primitives.push(k);
-                base=base/k;
+                base = base / k;
             }
-            else
-            { k++};
+            else {
+                k++
+            }
+            ;
         }
     }
+
     getPrimitives(base);
+    primitives.reverse();
     console.log(primitives);
-    var sum =0;
-    results = [];
-    for (i =0; i<primitives.length; i++) {
-        /*while (number >= primitives[i]) {
-            number = parseInt(number / primitives[i]);
-            sum = sum + number;
+    console.log(primitives.length);
+
+    var sum = 0;
+    var j=1;
 
 
+    for (i = 0; i < primitives.length; i++)
+    { //
 
+          while (number >= primitives[0]) {
+            number = parseInt(number/ primitives[i]);
+              sum = sum + number;
 
-        var count = 0;
-        var n = number;
-        while (n >= 1) {
-            var temp = Math.floor(n / i);
-            n = temp;
-            count += temp;
         }
-        results.push(Math.floor(count / primitives[i]));
+        if (primitives[i]==primitives[i+1])
+        {
+            j++;
+        }
+
+
+
     }
+    console.log(j, 'j');
+    console.log(sum, '=sum');
+    if (j>3){
+        return Math.floor(sum/j);
+    }
+    else { return sum;}
+}
 
-    return Math.min.apply(null, results);
-
-*/
 
